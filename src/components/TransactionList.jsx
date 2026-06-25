6fcaf788a2e0b8dc6f6c2afed997b7678dd0c626
YussaMoney <highshow74@gmail.com>
2026-06-25 21:38:59 +0100

    Modified TransactionList component: enhance layout and improve transaction display with additional styling

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,20 +1,36 @@
 export default function TransactionList({ transactions, deleteTransaction }) {
+  const date = new Date().toDateString();
   return (
     <section>
       <h2 className="history">Transaction History</h2>
       <ul className="transaction-list">
         {transactions.length === 0 ? (
-          <p>No transaction yet</p>
+          <p>No transaction yet!</p>
         ) : (
           transactions.map((transaction) => (
-            <li key={transaction.id}>
-              {transaction.description} - ₦{transaction.amount}
-              <button
-                type="button"
-                onClick={() => deleteTransaction(transaction.id)}
-              >
-                X
-              </button>
+            <li
+              className={`transaction-item ${
+                transaction.amount > 0 ? "income" : "expense"
+              }`}
+            >
+              <div className="transaction-description">
+                {transaction.description}{" "}
+                <span>
+                  <i>{date}</i>
+                </span>
+              </div>
+
+              <div className="amountWithDeletBtn">
+                <div className="transaction-amount">
+                  ₦{Math.abs(transaction.amount).toLocaleString()}
+                </div>
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTransaction(transaction.id)}
+                >
+                  ✕
+                </button>
+              </div>
             </li>
           ))
         )}
