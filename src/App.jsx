4ab23a5ab9bc544ba5d214328bd2e6ae22e67dfc
YussaMoney <highshow74@gmail.com>
2026-06-25 18:36:55 +0100

    Modified App component: utilize TransactionForm, Summary, and TransactionList components for improved structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import "../src/style.css";
+import TransactionList from "./components/TransactionList";
+import Summary from "./components/Summary";
+import TransactionForm from "./components/TransactionForm";
 
 function App() {
   const [description, setDescription] = useState("");
@@ -33,15 +37,6 @@ function App() {
     setTransactions(transactions.filter((item) => item.id !== id));
   }
 
-  let income = 0;
-  let expense = 0;
-  transactions.forEach((transaction) => {
-    transaction.amount > 0
-      ? (income += transaction.amount)
-      : (expense += transaction.amount);
-  });
-  const balance = income + expense;
-
   return (
     <div className="container">
       <header>
@@ -49,53 +44,18 @@ function App() {
       </header>
 
       <main>
-        <label htmlFor="desc">
-          Description:
-          <input
-            type="text"
-            name="desc"
-            id="desc"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <label htmlFor="amount">
-          Amount:
-          <input
-            type="number"
-            name="amount"
-            id="amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </label>
-
-        <button type="button" onClick={addTransaction}>
-          Add Transaction
-        </button>
-
-        <section>
-          <div className="income">Income: ₦{income}</div>
-          <div className="expense">Expense: ₦{expense}</div>
-          <div className="balance">Balance: ₦{balance}</div>
-        </section>
-
-        <section>
-          <h2 className="history">Transaction History</h2>
-          <ul className="transaction-list">
-            {transactions.map((transaction) => (
-              <li key={transaction.id}>
-                {transaction.description} - {transaction.amount}
-                <button
-                  type="button"
-                  onClick={() => deleteTransaction(transaction.id)}
-                >
-                  X
-                </button>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <TransactionForm
+          addTransaction={addTransaction}
+          setDescription={setDescription}
+          setAmount={setAmount}
+          description={description}
+          amount={amount}
+        />
+        <Summary transactions={transactions} />
+        <TransactionList
+          transactions={transactions}
+          deleteTransaction={deleteTransaction}
+        />
       </main>
     </div>
   );
